3a8f62040c6053728981885447abc1e6202e1213
davsun22daw <davsun22daw@proton.me>
2023-02-03 17:44:22 +0100

    Editar profe

--- src/Persona.java
+++ src/Persona.java
@@ -52,4 +52,5 @@ public class Persona {
                 ", email='" + email + '\'' +
                 '}';
     }
+
 }
--- src/main.java
+++ src/main.java
@@ -3,14 +3,141 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class main {
+    static ArrayList<Persona> profesors = new ArrayList<Persona>();
+    static ArrayList<Persona> alumnes = new ArrayList<Persona>();
+    private static void crearProfesor(Scanner input) {
+        boolean errorComprobacio = false;
+
+        System.out.println("Escriu el DNI del profesor: ");
+        String DNIProf = input.nextLine();
+
+        //Comproba numero de caracters
+        if (DNIProf.length() < 9 || DNIProf.length() >= 10) {
+            System.out.println("DNI INVALID");
+            return;
+        }
+
+        //Comproba el valor duplicat
+        Iterator<Persona> itComprobaDNI = profesors.iterator();
+        while (itComprobaDNI.hasNext()) {
+            Profesor prof = (Profesor) itComprobaDNI.next();
+            if (DNIProf.equals(prof.getDni())) {
+                System.out.println("EL DNI " + DNIProf + " ya existeix!");
+                errorComprobacio = true;
+                break;
+            }
+        }
+
+        if (errorComprobacio) {
+            return;
+        }
+
+        System.out.println("Escriu el nom del profesor: ");
+        String nomProf = input.nextLine();
+
+        //Comproba que el string no estigui buit
+        if (nomProf.isEmpty()) {
+            System.out.println("El nom no pot estar buit");
+            errorComprobacio = true;
+            return;
+        }
+
+        if (errorComprobacio) {
+            return;
+        }
+
+        System.out.println("Escriu el cognom del profesor: ");
+        String cognomProf = input.nextLine();
+
+        //Comproba que el string no estigui buit
+        if (cognomProf.isEmpty()) {
+            System.out.println("El cognom no pot estar buit");
+            errorComprobacio = true;
+            return;
+        }
+
+        if (errorComprobacio) {
+            return;
+        }
+
+        System.out.println("Escriu el email del profesor: ");
+        String emailProf = input.nextLine();
+
+        //Comproba el valor duplicat
+        Iterator<Persona> itComprobaMAIL = profesors.iterator();
+        while (itComprobaMAIL.hasNext()) {
+            Profesor prof = (Profesor) itComprobaMAIL.next();
+            if (emailProf.equals(prof.getEmail())) {
+                System.out.println("EL correu electronic " + emailProf + " ya existeix!");
+                errorComprobacio = true;
+                break;
+            }
+        }
+
+        if (errorComprobacio) {
+            return;
+        }
+
+        System.out.println("Escriu el id del profesor: ");
+        Integer idProf = input.nextInt();
+
+        //Comproba ID valor
+        if (idProf <= 0 || idProf >= 1000) {
+            System.out.println("ID Invalida! Escriu un numero del 1 al 999");
+            return;
+        }
+
+        //Comproba el valor duplicat
+        Iterator<Persona> itComprobaID = profesors.iterator();
+        while (itComprobaID.hasNext()) {
+            Profesor prof = (Profesor) itComprobaID.next();
+            if (idProf.equals(prof.getIdProfesor())) {
+                System.out.println("La ID " + idProf + " ya existeix!");
+                errorComprobacio = true;
+                break;
+            }
+        }
+
+        if (errorComprobacio) {
+            return;
+        }
+
+        System.out.println("Escriu la aula on es tutor el profesor: ");
+        Integer aulaProf = input.nextInt();
+
+        //Comproba el valor duplicat
+        Iterator<Persona> itComprobaAULA = profesors.iterator();
+        while (itComprobaAULA.hasNext()) {
+            Profesor prof = (Profesor) itComprobaAULA.next();
+            if (aulaProf.equals(prof.getTutorAula())) {
+                System.out.println("La aula " + aulaProf + " ya te un profesor!");
+                errorComprobacio = true;
+                break;
+            }
+        }
+
+        if (errorComprobacio) {
+            return;
+        }
+
+        System.out.println("Escriu quantes horas fara a la setmana ");
+        Double horesProf = input.nextDouble();
+        System.out.println("Escriu el salari que guanya per cada hora: ");
+        Double salariProf = input.nextDouble();
+
+
+        if (!errorComprobacio) {
+            Profesor profesor = new Profesor(DNIProf, nomProf, cognomProf, emailProf, idProf, aulaProf, horesProf, salariProf);
+            profesors.add(profesor);
+            System.out.println("Acció completada!");
+        }
+    }
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         int mainMenuChoice = 0;
         int subMenuChoice;
         Scanner sc = new Scanner(System.in);
-
-        ArrayList<Persona> profesors = new ArrayList<Persona>();
-        ArrayList<Persona> alumnes = new ArrayList<Persona>();
 
         // Per crear els valors
         Profesor profesor1 = new Profesor("98745034J", "Estela", "Simon", "esimon.clot@fje.edu", 001, 202, 40.0, 15.0);
@@ -51,148 +178,41 @@ public class main {
 
                             switch (subMenuChoice) {
                                 case 1:
-                                    boolean errorComprobacio = false;
                                     System.out.print("\n");
                                     System.out.println("Crear profesor ");
-
-                                    System.out.println("Escriu el DNI del profesor: ");
-                                    String DNIProf = input.nextLine();
-
-                                    //Comproba numero de caracters
-                                    if (DNIProf.length() < 9 || DNIProf.length() >= 10) {
-                                        System.out.println("DNI INVALID");
-                                        break;
-                                    }
-
-                                    //Comproba el valor duplicat
-                                    Iterator<Persona> itComprobaDNI = profesors.iterator();
-                                    while (itComprobaDNI.hasNext()) {
-                                        Profesor prof = (Profesor) itComprobaDNI.next();
-                                        if (DNIProf.equals(prof.getDni())) {
-                                            System.out.println("EL DNI " + DNIProf + " ya existeix!");
-                                            errorComprobacio = true;
-                                            break;
-                                        }
-                                    }
-
-                                    if (errorComprobacio) {
-                                        break;
-                                    }
-
-                                    System.out.println("Escriu el nom del profesor: ");
-                                    String nomProf = input.nextLine();
-
-                                    //Comproba que el string no estigui buit
-                                    if (nomProf.isEmpty()) {
-                                        System.out.println("El nom no pot estar buit");
-                                        errorComprobacio = true;
-                                        break;
-                                    }
-
-                                    if (errorComprobacio) {
-                                        break;
-                                    }
-
-                                    System.out.println("Escriu el cognom del profesor: ");
-                                    String cognomProf = input.nextLine();
-
-                                    //Comproba que el string no estigui buit
-                                    if (cognomProf.isEmpty()) {
-                                        System.out.println("El cognom no pot estar buit");
-                                        errorComprobacio = true;
-                                        break;
-                                    }
-
-                                    if (errorComprobacio) {
-                                        break;
-                                    }
-
-                                    System.out.println("Escriu el email del profesor: ");
-                                    String emailProf = input.nextLine();
-
-                                    //Comproba el valor duplicat
-                                    Iterator<Persona> itComprobaMAIL = profesors.iterator();
-                                    while (itComprobaMAIL.hasNext()) {
-                                        Profesor prof = (Profesor) itComprobaMAIL.next();
-                                        if (emailProf.equals(prof.getEmail())) {
-                                            System.out.println("EL correu electronic " + emailProf + " ya existeix!");
-                                            errorComprobacio = true;
-                                            break;
-                                        }
-                                    }
-
-                                    if (errorComprobacio) {
-                                        break;
-                                    }
-
-                                    System.out.println("Escriu el id del profesor: ");
-                                    Integer idProf = input.nextInt();
-
-                                    //Comproba ID valor
-                                    if (idProf <= 0 || idProf >= 1000) {
-                                        System.out.println("ID Invalida! Escriu un numero del 1 al 999");
-                                        break;
-                                    }
-
-                                    //Comproba el valor duplicat
-                                    Iterator<Persona> itComprobaID = profesors.iterator();
-                                    while (itComprobaID.hasNext()) {
-                                        Profesor prof = (Profesor) itComprobaID.next();
-                                        if (idProf.equals(prof.getIdProfesor())) {
-                                            System.out.println("La ID " + idProf + " ya existeix!");
-                                            errorComprobacio = true;
-                                            break;
-                                        }
-                                    }
-
-                                    if (errorComprobacio) {
-                                        break;
-                                    }
-
-                                    System.out.println("Escriu la aula on es tutor el profesor: ");
-                                    Integer aulaProf = input.nextInt();
-
-                                    //Comproba el valor duplicat
-                                    Iterator<Persona> itComprobaAULA = profesors.iterator();
-                                    while (itComprobaAULA.hasNext()) {
-                                        Profesor prof = (Profesor) itComprobaAULA.next();
-                                        if (aulaProf.equals(prof.getTutorAula())) {
-                                            System.out.println("La aula " + aulaProf + " ya te un profesor!");
-                                            errorComprobacio = true;
-                                            break;
-                                        }
-                                    }
-
-                                    if (errorComprobacio) {
-                                        break;
-                                    }
-
-                                    System.out.println("Escriu quantes horas fara a la setmana ");
-                                    Double horesProf = input.nextDouble();
-                                    System.out.println("Escriu el salari que guanya per cada hora: ");
-                                    Double salariProf = input.nextDouble();
-
-
-                                    if (!errorComprobacio) {
-                                        Profesor profesor = new Profesor(DNIProf, nomProf, cognomProf, emailProf, idProf, aulaProf, horesProf, salariProf);
-                                        profesors.add(profesor);
-                                        System.out.println("Profesor creat amb exit!");
-                                    }
+                                    crearProfesor(input);
                                     break;
                                 case 2:
-                                    Profesor profesor = (Profesor) profesors.get(0);
-                                    profesor.setEmail("newemail@fje.edu");
-                                    profesors.set(0, profesor);
+                                    System.out.println("Introduce el ID del profesor a editar: ");
+                                    int idProfAEditar = input.nextInt();
+                                    input.nextLine();
+                                    boolean profesorEditado = false;
+                                    Iterator<Persona> editar = profesors.iterator();
+                                    while (editar.hasNext()){
+                                        Profesor prof = (Profesor) editar.next();
+                                        if (prof.getIdProfesor() == idProfAEditar) {
+                                            editar.remove();
+                                            profesorEditado = true;
+                                            break;
+                                        }
+                                    }
+                                    if (profesorEditado) {
+                                        crearProfesor(input);
+                                        System.out.println("El profesor con ID " + idProfAEditar + " ha sido editado.");
+                                    } else {
+                                        System.out.println("No se ha encontrado un profesor con ID " + idProfAEditar + ".");
+                                    }
                                     break;
+
                                 case 3:
                                     System.out.println("Introduce el ID del profesor a eliminar: ");
                                     int idProfAEliminar = input.nextInt();
                                     boolean profesorEliminado = false;
-                                    Iterator<Persona> it = profesors.iterator();
-                                    while (it.hasNext()){
-                                        Profesor prof = (Profesor) it.next();
+                                    Iterator<Persona> borrar = profesors.iterator();
+                                    while (borrar.hasNext()){
+                                        Profesor prof = (Profesor) borrar.next();
                                         if (prof.getIdProfesor() == idProfAEliminar) {
-                                            it.remove();
+                                            borrar.remove();
                                             profesorEliminado = true;
                                             break;
                                         }
